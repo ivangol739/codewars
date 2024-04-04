b62deaee2f97c26d3d5873a731d04e7a2ea7b635
ivangol739 <ivangol739@gmail.com>
2024-04-04 22:55:40 +0300

    first commit

---
+++ Create Phone Number.py
@@ -0,0 +1,18 @@
+# Write a function that accepts an array of 10 integers (between 0 and 9),
+# that returns a string of those numbers in
+# the form of a phone number.
+
+def create_phone_number(numbers):
+    # Проверяем, что входной массив содержит ровно 10 элементов
+    if len(numbers) != 10:
+        return "Массив должен содержать ровно 10 чисел"
+
+    # Форматируем номер телефона в соответствии с требованиями
+    phone_number = "({}{}{}) {}{}{}-{}{}{}{}".format(*numbers)
+
+    return phone_number
+
+
+# Пример использования
+phone = create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+print(phone)  # Выведет: "(123) 456-7890"
