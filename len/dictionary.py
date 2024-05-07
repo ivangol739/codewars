fd5e4a4375bff03f39c696e21fd934c6ad041252
ivangol739 <ivangol739@gmail.com>
2024-05-07 23:20:55 +0300

    com d1

---
+++ len/dictionary.py
@@ -0,0 +1,47 @@
+# pythons = {
+#   'Chapman': 'Graham',
+#   'Cleese': 'John',
+#   'Idle': 'Eric',
+#   'Jones': 'Terry',
+#   'Palin': 'Michael',
+# }
+
+# pythons['Gilliam'] = 'Gerry'
+# print(pythons)
+# pythons['Gilliam'] = 'Terry'
+# print(pythons)
+# a = pythons['Chapman']
+# print(a)
+# b = pythons.get('Jones')
+# print(b)
+# all_keys = list(pythons.keys())
+# print(all_keys)
+# all_value = list(pythons.values())
+# print(all_value)
+# all_keys_values = list(pythons.items())
+# print(all_keys_values)
+
+# print(len(pythons))
+
+# del(pythons['Cleese'])
+# print(pythons)
+
+print(set({'apple': 'red', 'orange': 'orange', 'cherry': 'red'}))
+s = set((1, 2, 3, 'eee'))
+print(s)
+s.add(5)
+print(s)
+s.remove('eee')
+print(s)
+
+furniture = set(('sofa', 'ottoman', 'table'))
+for i in furniture:
+  print(i)
+
+drinks = {
+  'martini': {'vodka', 'vermouth'},
+  'black russian': {'vodka', 'kahlua'},
+  'white russian': {'cream', 'kahlua', 'vodka'},
+  'manhattan': {'rye', 'vermouth', 'bitters'},
+  'screwdriver': {'orange juice', 'vodka'}
+}
