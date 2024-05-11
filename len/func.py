5ce3bae979bde4d8f3f64bbd2b54d68c5e8b927d
ivangol739 <ivangol739@gmail.com>
2024-05-11 23:17:35 +0300

    lymbda

--- len/func.py
+++ len/func.py
@@ -111,6 +111,23 @@ print(a())
 b = knights2('Hasenpfeffer')
 print(b())
 
+# ---
+
+def edit_story(words, func):
+  for word in words:
+    print(func(word))
+
+stairs = ['thud', 'meow', 'thud', 'hiss']
+
+def enliven(word):
+  return word.capitalize() + '!'
+
+edit_story(stairs, enliven)
+
+edit_story(stairs, lambda word: word.capitalize() + '!')
+
+
+
 
 
 
