c32df612e77174d35c9d1d0d138efce82deb3a63
ivangol739 <ivangol739@gmail.com>
2024-05-20 21:58:54 +0300

    list_of_numbers

--- Types.py
+++ Types.py
@@ -6,7 +6,7 @@ def check_email(email: str) -> bool:
   else:
     return False
 
-print(check_email('em@il.ru'))
+# print(check_email('em@il.ru'))
 
 
 def longest_film(film_1, film_2, film_3):
@@ -14,13 +14,13 @@ def longest_film(film_1, film_2, film_3):
   res = max(films)
   return res
 
-print(longest_film('Аладин', 'Мадагаскар', 'Бетховен'))
+# print(longest_film('Аладин', 'Мадагаскар', 'Бетховен'))
 
 
 def string_slices(string: str):
   return string[2: len(string) - 2]
 
-print(string_slices('%%1gtfgeghethte1&#'))
+# print(string_slices('%%1gtfgeghethte1&#'))
 
 
 from typing import List
@@ -28,4 +28,10 @@ from typing import List
 def fio(initials: List[str]):
   return initials[0][0] + initials[1][0] + initials[2][0]
 
-print(fio(['Иванов', 'Иван', 'Иванович']))
+# print(fio(['Иванов', 'Иван', 'Иванович']))
+
+
+def list_of_numbers(n: int):
+  return list(range(1, n + 1))
+
+print(list_of_numbers(9))
