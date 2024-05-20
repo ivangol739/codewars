f03c93c9878ffff3ceac012f311a53866be6bb9d
ivangol739 <ivangol739@gmail.com>
2024-05-20 21:54:39 +0300

    string_slices

--- Types.py
+++ Types.py
@@ -9,10 +9,15 @@ def check_email(email: str) -> bool:
 print(check_email('em@il.ru'))
 
 
-
 def longest_film(film_1, film_2, film_3):
   films = [film_1, film_2, film_3]
   res = max(films)
   return res
 
 print(longest_film('Аладин', 'Мадагаскар', 'Бетховен'))
+
+
+def string_slices(string: str):
+  return string[2: len(string) - 2]
+
+print(string_slices('%%1gtfgeghethte1&#'))
