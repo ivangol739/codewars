52588fc5efe0b36ce4a5c494ad8a3afda2ede673
ivangol739 <ivangol739@gmail.com>
2024-06-03 21:56:28 +0300

    isintance

---
+++ len/rec.py
@@ -0,0 +1,11 @@
+def flatten(lol):
+  for item in lol:
+    if isinstance(item, list):
+      for subitem in flatten(item):
+        yield subitem
+    else:
+      yield item
+      
+lol = [1, 2, [3,4,5], [6,[7,8,9], []]]
+dlol = [1, 2, [3,4,5], [6,[7,8,9], []]]
+print(list(flatten(lol)))
