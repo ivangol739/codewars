38dba484ed5539712bfb3485fddb0837fda37112
ivangol739 <ivangol739@gmail.com>
2024-06-05 23:04:10 +0300

    наследование

--- len/class.py
+++ len/class.py
@@ -1,15 +1,36 @@
+# class Cat():
+#   pass
+
+# a_cat = Cat()
+# a_another_cat = Cat()
+
+# a_cat.age = 3
+# a_cat.name = "Luci"
+
+# a_cat.nemesis = a_another_cat
+# a_cat.nemesis.name = "dodi"
+
+
 class Cat():
+  def __init__(self, name):
+    self.name = name
+    
+furball = Cat('DOD')
+# print(furball.name)
+
+class Car():
+  def exclaim(self):
+    print("I'm Car")
+
+class Yugo(Car):
   pass
 
-a_cat = Cat()
-a_another_cat = Cat()
+# print(issubclass(Yugo, Car))
 
-a_cat.age = 3
-a_cat.name = "Luci"
+give_me_a_car = Car()
+give_me_a_yogo = Yugo()
 
-a_cat.nemesis = a_another_cat
-a_cat.nemesis.name = "dodi"
-
-
+give_me_a_car.exclaim()
+give_me_a_yogo.exclaim()
 
 
