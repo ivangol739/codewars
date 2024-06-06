2a2c26ac71fe10784be6c62e9985aac89907faa3
ivangol739 <ivangol739@gmail.com>
2024-06-06 23:34:36 +0300

    Геттеры и сеттеры 1

--- len/class.py
+++ len/class.py
@@ -89,7 +89,27 @@ class Mule(Donkey, Horse):
 class Hinny(Horse, Donkey):
   pass
 
+mule = Mule()
+hinny = Hinny()
 
+# print(mule.says())
+# print(hinny.says())
 
+# Геттеры и сеттеры
+class Duck():
+  def __init__(self, input_name):
+    self.hidden_name = input_name
+  def get_name(self):
+    print("inside the getter")
+    return self.hidden_name
+  def set_name(self, input_name):
+    print("inside the setter")
+    self.hidden_name = input_name
+    
+don = Duck("Donald")
+print(don.get_name())
+
+don.set_name('Donna')
+print(don.get_name())
 
 
