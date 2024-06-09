c3646570ec6806400c06aa8f9609eec45ba143e3
ivangol739 <ivangol739@gmail.com>
2024-06-09 19:02:20 +0300

    метод str

--- len/class.py
+++ len/class.py
@@ -207,9 +207,31 @@ class Word():
   def equals(self, word2):
     return self.text.lower() == word2.text.lower()
   
+class Word():
+  def __init__(self, text):
+    self.text = text
+    
+  def __eq__(self, word2):
+    return self.text.lower() == word2.text.lower()
+  
 first = Word('ha')
 second = Word('HA')
 third = Word('eh')
 
-print(first.equals(second))
-print(first.equals(third))
+# print(first==second)
+# print(first==third)
+
+# print(first.equals(second))
+# print(first.equals(third))
+
+# Метод __str__
+class Word():
+  def __init__(self, text):
+    self.text = text
+  def __eq__(self, word2):
+    return self.text.lower() == word2.text.lower()
+  def __str__(self):
+    return self.text
+  
+first = Word('ha')
+# print(first)
