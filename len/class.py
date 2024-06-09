3bd0d5dac54974b06112e727161e842a1dc916b1
ivangol739 <ivangol739@gmail.com>
2024-06-09 17:10:03 +0300

    Утианя типизация

--- len/class.py
+++ len/class.py
@@ -149,5 +149,35 @@ class Fruit:
   color = 'red'
   
 bluberry = Fruit()
-print(bluberry.color)
-print(Fruit.color)
+# print(bluberry.color)
+# print(Fruit.color)
+
+#Утинаная типипзация - полиморфизм
+
+class Quote():
+  def __init__(self, person, words):
+    self.person = person
+    self.words = words
+    
+  def who(self):
+    return self.person
+  
+  def says(self):
+    return self.words + "."
+  
+class QuestionQuote(Quote):
+  def says(self):
+    return self.words + "?"
+  
+class ExclamationQuote(Quote):
+  def says(self):
+    return self.words + "!"
+  
+hunter = Quote("Elmer Fudd", "I'm hunting wabbits")
+print(hunter.who(), "says:", hunter.says())
+
+hunter1 = QuestionQuote("Bugs Bunny", "What's up, doc")
+print(hunter1.who(), "says:", hunter1.says())
+
+hunter2 = QuestionQuote("Daffy Duck", "It's rabbit season")
+print(hunter2.who(), "says:", hunter2.says())
