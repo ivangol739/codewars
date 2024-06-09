adab7af2862cd935022bbe1590ff1e46fd76205f
ivangol739 <ivangol739@gmail.com>
2024-06-09 17:30:50 +0300

    магические методы 1

--- len/class.py
+++ len/class.py
@@ -198,3 +198,18 @@ def who_says(obj):
 who_says(hunter)
 who_says(hunter1)
 who_says(hunter2)
+
+# Магические метода
+class Word():
+  def __init__(self, text):
+    self.text = text
+    
+  def equals(self, word2):
+    return self.text.lower() == word2.text.lower()
+  
+first = Word('ha')
+second = Word('HA')
+third = Word('eh')
+
+print(first.equals(second))
+print(first.equals(third))
