41aee9e7157000ce04c31a33c14bfa89aed6d875
ivangol739 <ivangol739@gmail.com>
2024-07-30 18:35:35 +0300

    xml dont com

--- Neto/formatfiles/forfiles.py
+++ Neto/formatfiles/forfiles.py
@@ -8,57 +8,57 @@ file1 = os.path.join(os.getcwd(), "Neto", "formatfiles", "fileslections", "newsa
 file2 = os.path.join(os.getcwd(), "Neto", "formatfiles", "fileslections", "newsafr.json")
 file3 = os.path.join(os.getcwd(), "Neto", "formatfiles", "fileslections", "newsafr.xml")
 
-##ЧТЕНИЕ CSV ФАЙЛА ПОСТРОЧНО ЛЮБОЙ ДЛИНЫ
-# with open(file1, newline="", encoding="UTF-8") as f:
-#   reader = csv.reader(f)
-#   count = -1
-#   for row in reader:
-#     if count >= 0:
-#       print(row[-1])
-#     count += 1
+#ЧТЕНИЕ CSV ФАЙЛА ПОСТРОЧНО ЛЮБОЙ ДЛИНЫ
+with open(file1, newline="", encoding="UTF-8") as f:
+  reader = csv.reader(f)
+  count = -1
+  for row in reader:
+    if count >= 0:
+      print(row[-1])
+    count += 1
 
-##ЧТЕНИЕ ФАЙЛА ЦЕЛИКОМ
-# with open(file1, newline="", encoding="UTF-8") as f:
-#   reader = csv.reader(f)
-#   new_list = list(reader)
+#ЧТЕНИЕ ФАЙЛА ЦЕЛИКОМ
+with open(file1, newline="", encoding="UTF-8") as f:
+  reader = csv.reader(f)
+  new_list = list(reader)
   
 #Убрать заголовок
-# header = new_list.pop(0)
+header = new_list.pop(0)
   
 #ЗАПИСЬ ФАЙЛА
-# csv.register_dialect("comma_no_quoting", delimiter=",", quoting=csv.QUOTE_NONE, escapechar="\\")
-# csv.register_dialect("semicolon_quote_all", delimiter=";", quoting=csv.QUOTE_ALL)
+csv.register_dialect("comma_no_quoting", delimiter=",", quoting=csv.QUOTE_NONE, escapechar="\\")
+csv.register_dialect("semicolon_quote_all", delimiter=";", quoting=csv.QUOTE_ALL)
 
-# with open(os.path.join(os.getcwd(), "Neto", "formatfiles", "fileslections", "result.csv"), "w", encoding="UTF-8", newline="") as f:
-#   writer = csv.writer(f, dialect="semicolon_quote_all")
-#   writer.writerow(header)
-#   writer.writerows(new_list[:3])
+with open(os.path.join(os.getcwd(), "Neto", "formatfiles", "fileslections", "result.csv"), "w", encoding="UTF-8", newline="") as f:
+  writer = csv.writer(f, dialect="semicolon_quote_all")
+  writer.writerow(header)
+  writer.writerows(new_list[:3])
   
   
-##Чтение файла в словарь
-# with open(file1, newline="", encoding="UTF-8") as f:
-#   reader = csv.DictReader(f)
-#   for row in reader:
-#     print(row["title"])
+#Чтение файла в словарь
+with open(file1, newline="", encoding="UTF-8") as f:
+  reader = csv.DictReader(f)
+  for row in reader:
+    print(row["title"])
 
 
 
-##ЧТЕНИЕ JSON 
-# with open(file2, encoding="UTF-8") as f:
-#   json_data = json.load(f)
+#ЧТЕНИЕ JSON 
+with open(file2, encoding="UTF-8") as f:
+  json_data = json.load(f)
   
-# new_list = json_data["rss"]["channel"]["items"]
+new_list = json_data["rss"]["channel"]["items"]
 
-# for row in new_list:
-#   print(row["title"])
+for row in new_list:
+  print(row["title"])
   
-##ЗАПИСЬ JSON
-# with open(os.path.join(os.getcwd(), "Neto", "formatfiles", "fileslections", "resultJSON.json"), "w", encoding="UTF-8", newline="") as f:
-#   json.dump(new_list, f, ensure_ascii=False, indent=4)
+#ЗАПИСЬ JSON
+with open(os.path.join(os.getcwd(), "Neto", "formatfiles", "fileslections", "resultJSON.json"), "w", encoding="UTF-8", newline="") as f:
+  json.dump(new_list, f, ensure_ascii=False, indent=4)
 
 
 
-##СОЗДАЕМ ПАРСЕР И ЧИТАЕМ XML
+#СОЗДАЕМ ПАРСЕР И ЧИТАЕМ XML
 import xml.etree.ElementTree as ET
 
 parser = ET.XMLParser(encoding="UTF-8")
@@ -74,7 +74,7 @@ for item in new_list:
   title = item.find("title")
   print(title.text)
   
-##ЗАПИСЬ XML
+#ЗАПИСЬ XML
 tree.write(os.path.join(os.getcwd(), "Neto", "formatfiles", "fileslections", "resultXML.xml"), encoding="UTF-8")
 
 #дгугой пример
